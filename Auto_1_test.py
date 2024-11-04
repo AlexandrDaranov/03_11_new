8c5e9d42979584f8870344c2c9201e76c2c70660
AlexandrDaranov <alexdvvv15@gmail.com>
2024-11-04 12:18:48 +0400

    3

--- Auto_1_test.py
+++ Auto_1_test.py
@@ -1,6 +1,17 @@
-import selenium
+#import selenium
 '''Почему не добавляется модуль selenium?'''
 
+#@pytest.fixture()
+def before_after():
+    print("Before test")
+    yield
+    print("\nAfter test")
+
+def test_demo1():
+    assert 1 == 1
+
+def test_demo2(before_after):
+    assert 1 == 2
 
 
 
