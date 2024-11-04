1a888ebd07a371b012c16ddb1c5287ceef1cf529
AlexandrDaranov <alexdvvv15@gmail.com>
2024-11-04 21:49:03 +0400

    04_11

--- Auto_2_site_test.py
+++ Auto_2_site_test.py
@@ -1,9 +1,7 @@
-#from distutils.command.install import value
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
-
+import time
 
 @pytest.fixture()
 def browser():
@@ -13,9 +11,17 @@ def browser():
     yield browser
     browser.close()     #постусловие для закрытия браузера после теста
 
-def test_open_ip6(browser):
+def test_open_ip6(browser):                        #Проверка названия карточки на доп странице
     browser.get('https://demoblaze.com/index.html')
     iphone_6 = browser.find_element(By.XPATH, value = '//a[text()="Iphone 6 32gb"]')
     iphone_6.click()
     title = browser.find_element(By.CSS_SELECTOR, value = 'h2')
     assert title.text == 'Iphone 6 32gb'
+
+def test_two_monitors(browser):                     #Проверка наличия 2 карточек сразу на доп странице
+    browser.get('https://demoblaze.com/index.html')
+    monitor_link = browser.find_element(By.CSS_SELECTOR, value = '''[onclick="byCat('monitor')"]''')
+    monitor_link.click()
+    time.sleep(2) #это крайняя мера, в других случ. исп. конкретное ожидание чего-то
+    monitors = browser.find_elements(By.CSS_SELECTOR, value = ".card")
+    assert len(monitors) == 2
